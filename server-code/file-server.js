eae494e90dbea561277468f646c01595780aa128
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2018-12-16 15:52:41 +0530

    final working server with correct conent type for each file type

--- server-code/file-server.js
+++ server-code/file-server.js
@@ -1,29 +1,63 @@
-module.exports = function(){
+/** file-server module
+ * to provide the request file to the webpage with proper content type based on file extension
+ * @param fileName (file to be served)
+ * @param request (request received from http server)
+ * @param response (response to be provided to http server)
+ **/
+module.exports = function(fileName, request, response){
 	//import file system module
 	const fs = require("fs");
-	//variable to store fileName
-	let fileName;
+	//import path module
+	const path = require("path");
+	//variable to store rootDirectory
+	let root = "../application-code";
+	//variable to store complete file path
+	let filepath;
 	//variable to store content-type
 	let type;
-	//variable to store extensions
-	const extensions = {
-		"html":"text/html",
-		"js":"text/javascript",
-		"css":"text/css",
-		"json":"text/json",
-		"png":"image/png",
-		"jpg":"image/jpg",
-		"wav":"audio/wav"
+	//generate the full file path of the file requested and set the type for content based on extension
+	if(fileName === "/index.html"){
+		filepath = root + fileName;
+		type = "text/html";
 	}
-	//callback function on server hit
-	function onRequest(request,response){
-		//check the requested url and serve accordingly the file type
-		if(request.url === "/" || request.url === "/index.html"){
-			fileName = "./index.html"
-			type = "text/html"
+	else{
+		switch (path.extname(fileName)) {
+			case ".html":
+				filepath = root + fileName;
+				type = "text/html";
+				break;
+			case ".js":
+				filepath = root + fileName;
+				type = "text/javascript";
+				break;
+			case ".css":
+				filepath = root + fileName;
+				type = "text/css";
+				break;
+			case ".json":
+				filepath = root + fileName;
+				type = "text/json";
+				break;
+			case ".jpg":
+				type = "image/jpg";
+			case ".png":
+				filepath = root + fileName;
+				type = "image/png";
+				break;
+			case ".wav":
+				filepath = root + fileName;
+				type = "audio/wav";
+				break;
+			default:
+				filepath = null;
+				break;
 		}
-		//read file present at relative path and execute callback
-		fs.readFile(fileName,
+	}
+	/**
+	 *  if filepath is valid then read the file fromthe location and give it in response
+	 **/
+	if(filepath){
+		fs.readFile(filepath,
 			/**
 			 * @callback
 			 * @param err (if any error while reading file return true)
@@ -37,6 +71,9 @@ module.exports = function(){
 					response.write(data);
 					//close response
 					response.end();
+				}
+				else{
+					console.log(`File : ${fileName} does not exist.\nThe complete filepath is ${filepath}`);
 				}
 			}
 		);
--- server-code/server-start.js
+++ server-code/server-start.js
@@ -2,15 +2,22 @@
 const http = require("http");
 //import fileserver module
 const fileServer = require("./file-server");
-//variable to store filename requested
-let fileName;
 /**
- * create http server to listen on port
- **/
+ * create http server to listen on port 8888
+ **/ 
 http.createServer(function(request, response){
+	//variable to store file to be served
+	let fileName = request.url;
+	//if root url requested then serve index.html
 	if(request.url === "/"){
-		fileName = "../application-code/index.html"
+		fileName = "/index.html"
 	}
+	/** 
+	 * call module file-server with the required arguments
+	 * @argument fileName (file to be served)
+	 * @argument request (request of the http server from the webpage)
+	 * @argument response (response to be provided to the webpage by the http server)
+	**/
 	fileServer(fileName, request, response)
 }).listen(8888);
 
